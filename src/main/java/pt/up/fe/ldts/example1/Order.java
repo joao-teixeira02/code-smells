edf2eb342a3ed340f2d4070e9e98dd2e639a4044
JoAPSo Pedro Reis Teixeira <up202005437@fe.up.pt>
2021-12-10 17:03:33 +0000

    Example 1

--- src/main/java/pt/up/fe/ldts/example1/Order.java
+++ src/main/java/pt/up/fe/ldts/example1/Order.java
@@ -14,29 +14,22 @@ public class Order {
         lines.add(new OrderLine(product, quantity));
     }
 
-    public boolean isElegibleForFreeDelivery() {
+    private double getTotal() {
         double total = 0;
-
         for (OrderLine line : lines)
-            total += line.product.getPrice() * line.quantity;
+            total += line.getTotal();
+        return total;
+    }
 
-        if (total > 100) return true;
-        else return false;
+    public boolean isElegibleForFreeDelivery() {
+        return getTotal() > 100;
     }
 
     public String printOrder() {
         StringBuffer printBuffer = new StringBuffer();
-
         for (OrderLine line : lines)
-            printBuffer.append(line.product.getName() + "(x" + line.quantity + "): " + (line.product.getPrice() * line.quantity) + "\n");
-
-        double total = 0;
-
-        for (OrderLine line : lines)
-            total += line.product.getPrice() * line.quantity;
-
-        printBuffer.append("Total: " + total);
-
+            printBuffer.append(line);
+        printBuffer.append("Total: " + getTotal());
         return printBuffer.toString();
     }
 }
--- src/main/java/pt/up/fe/ldts/example1/OrderLine.java
+++ src/main/java/pt/up/fe/ldts/example1/OrderLine.java
@@ -8,4 +8,28 @@ public class OrderLine {
         this.product = product;
         this.quantity = quantity;
     }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return getProduct().getPrice() * getQuantity();
+    }
+
+    public String toString() {
+        return product.getName() + "(x" + quantity + "): " + (product.getPrice() * quantity) + "\n";
+    }
 }
